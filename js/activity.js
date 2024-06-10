4883111b50523cc1ce24e2e9561ee66f15167939
mrlathiya286 <mauliklathiya25@gmail.com>
2024-06-09 20:55:32 -0700

    Week 5 day 3 homework

--- js/activity.js
+++ js/activity.js
@@ -8,3 +8,30 @@ $(document).ready(function () {
     }
   });
 });
+
+$(document).ready(function () {
+  $("td").click(function () {
+    var content = $(this).text();
+    var columnIndex = $(this).index(); // Get the column index of the clicked cell
+    var cliffSiteName = $("table th").eq(columnIndex).text(); // Get the cliff site name based on the column index
+
+    if (content != "Not Available") {
+      $(this).toggleClass("tdhighlight");
+      if ($(this).hasClass("tdhighlight")) {
+        $("#displaySelected").css("visibility", "visible");
+        $("#displaySelected").css("margin-top", "2em");
+        if (!$("#result p:contains(" + content + ")").length) {
+          $("#result").append(
+            "<p>" + content + " at  " + cliffSiteName + "</p>"
+          );
+        }
+      } else {
+        $("#result p:contains(" + content + ")").remove();
+        if ($("#result").has("p").length == false) {
+          $("#displaySelected").css("visibility", "hidden");
+          $("#displaySelected").css("margin-top", "0");
+        }
+      }
+    }
+  });
+});
